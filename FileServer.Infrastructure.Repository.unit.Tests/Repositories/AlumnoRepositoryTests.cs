b7f1e7a85533c542cf03483a10ea7d2abf8a9f8e
COVALCODOM\gerard_serra <gserra@covalco.es>
2018-06-29 12:00:19 +0200

    Initial commit

---
+++ FileServer.Infrastructure.Repository.unit.Tests/Repositories/AlumnoRepositoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileServer.Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileServer.Infrastructure.Repository.Tests
+{
+    [TestClass()]
+    public class AlumnoRepositoryTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ FileServer.Infrastructure.Repository/Repositories/AlumnoRepository.cs
@@ -0,0 +1,17 @@
+﻿using FileServer.Common.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileServer.Infrastructure.Repository
+{
+    public class AlumnoRepository : IAlumnoRepository
+    {
+        public Alumno Add(Alumno alumno)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
